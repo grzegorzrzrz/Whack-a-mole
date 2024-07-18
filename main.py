6b0717e7a3acb90a87949528a02044fe9f1c4f8d
Grzegorz <grzegorz.siudak.stud@pw.edu.pl>
2024-07-18 17:57:55 +0200

    threading approach

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 import pyautogui
 import mss
 import keyboard
+import threading
+import time
 
 template_paths = [
     "match_1.png",
@@ -10,7 +12,7 @@ template_paths = [
     "match_3.png",
     "match_4.png"
 ]
-monitor = {"top": 323, "left": 267, "width": 1327, "height": 598}
+monitor = {"top": 240, "left": 410, "width": 1140, "height": 580}
 
 templates = []
 template_dimensions = []
@@ -21,25 +23,32 @@ for path in template_paths:
         h, w = template_img.shape[:2]
         template_dimensions.append((w, h))
 
-threshold = 0.5
+threshold = 0.6
+
+lock = threading.Lock()
 
 def perform_template_matching(sct):
     input_img = np.array(sct.grab(monitor))
 
-    for i, (template_img, (w, h)) in enumerate(zip(templates, template_dimensions)):
-        result = cv2.matchTemplate(input_img, template_img, cv2.TM_CCOEFF_NORMED)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-        print(max_val)
-        if max_val > threshold:
-            x, y = max_loc
-            center_x = monitor['left'] + x + int(w / 2)
-            center_y = monitor['top'] + y + int(h / 2)
-            pyautogui.moveTo(center_x, center_y)
-            if i == 0:
-                pyautogui.doubleClick()
-            else:
-                pyautogui.click()
-            break
+    threads = []
+    for template_img, (w, h) in zip(templates, template_dimensions):
+        thread = threading.Thread(target=match_and_click, args=(input_img, template_img, w, h))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+def match_and_click(input_img, template_img, w, h):
+    result = cv2.matchTemplate(input_img, template_img, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+    if max_val > threshold:
+        x, y = max_loc
+        center_x = monitor['left'] + x + int(w / 2)
+        center_y = monitor['top'] + y + int(h / 2)
+        pyautogui.click(center_x, center_y)
+        pyautogui.click(center_x, center_y)
 
 def main():
     print("Press 'e' to start")
@@ -47,10 +56,17 @@ def main():
     print("Press 'q' to quit.")
 
     sct = mss.mss()
+
     while True:
+        start_time = time.time()
         perform_template_matching(sct)
+
         if keyboard.is_pressed('q'):
             break
+
+        elapsed_time = time.time() - start_time
+        if elapsed_time < 0.5:
+            time.sleep(0.5 - elapsed_time)
 
 if __name__ == '__main__':
     main()
